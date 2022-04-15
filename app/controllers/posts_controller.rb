93628eb107ee34fd74af5becaee61c6e9fd1fd3f
Jolly Yau <Jolly@MacBook-Pro-15-Retina.local>
2022-04-15 22:17:42 +1000

    changed text and background

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,29 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find params[:id]
+    @post.user_id = @current_user.id
+  end
+
+  def update
+    post = Post.find params[:id]
+    post.user_id = @current_user.id
+    if params[:file].present?
+      req = Cloudinary::Uploader.upload(params[:file])
+      post.image = req["public_id"]
+    end
+    post.update_attributes(post_params)
+    post.save
+    redirect_to root_path
+  end
+
+  def destroy
+    post = Post.find params[:id]
+    post.destroy
+    redirect_to root_path
+  end
+
   private
   def post_params #strong params
     params.require(:post).permit(:title, :post_text, :image, :video) #asks for params without getting directly from params object
